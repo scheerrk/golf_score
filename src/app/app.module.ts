38eb932770fce9743e3aae9f7788826c6ede34ca
rkscheer <rkscheer@mail.mccneb.edu>
2021-04-11 22:35:25 -0500

    committing to see creds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    RouterModule.forRoot([
+      {path: '', component: LoginComponent},
+      {path: 'home', component: HomeComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
